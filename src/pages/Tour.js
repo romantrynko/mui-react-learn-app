3f91414b1d5527132292631df6f8898431236531
Roman Trynko <trynkoroman25@gmail.com>
2022-08-26 16:55:39 +0300

    added accordion

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,4 +1,5 @@
 import { Container, Typography, Box } from '@mui/material';
+import CustomizedAccordions from './Accordion';
 import ImageCollage from './ImageCollage';
 
 const Tour = () => {
@@ -13,7 +14,26 @@ const Tour = () => {
           height={225}
           alt="img"
         ></img>
-        <ImageCollage/>
+        <ImageCollage />
+      </Box>
+      <Box>
+        <Typography variant="h6" component="h4" marginTop={3}>
+          About this ticket
+        </Typography>
+        <Typography variant="paragraph" component="p" marginTop={3}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac
+          nunc ante. In dictum libero sit amet massa lobortis, a sodales purus
+          pharetra. Etiam eget libero massa. Donec a sollicitudin nunc, eget
+          commodo dui. Nam erat lorem, cursus eget lacus sed, eleifend tempor
+          felis. In libero ipsum, dignissim quis vehicula eu, interdum ac
+          ligula. Sed at purus quis lectus mollis porta.
+        </Typography>
+      </Box>
+      <Box>
+        <Typography variant="h6" component="h4" marginTop={3}>
+          Frequently asked question
+        </Typography>
+        <CustomizedAccordions/>
       </Box>
     </Container>
   );
